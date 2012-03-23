af836f6b315cc619594a364b22c8b524528d1725
svanur@tern.is <svanur@tern.is@55e1ce28-8854-1d50-06f0-d29166e70782>
2012-03-23 01:30:50 +0000

    Simple physx car created.  Needs a lot of tweaking and the tires are not getting correctly updated from the physical PhysxWheelShape behind them. Added a simple keycontrol class to be able to control the car a little bit and skydome for cosmetics.

--- trunk/default.py
+++ trunk/default.py
@@ -1,41 +1,78 @@
 import direct.directbase.DirectStart 
-from panda3d.core import Vec3
-from panda3d.bullet import *
+from panda3d.core import *
+from panda3d.physx import PhysxManager
+from panda3d.physx import PhysxEnums
+from panda3d.physx import PhysxSceneDesc
+from panda3d.physx import PhysxBodyDesc
+from panda3d.physx import PhysxActorDesc
+from panda3d.physx import PhysxBoxShapeDesc
+from panda3d.physx import PhysxPlaneShapeDesc
+from panda3d.physx import PhysxPointOnLineJointDesc
 
+from skydome import SkyDome
 from Car import Car
+from keycontrol import KeyControl
 
 class World:
     """ """
     
     def __init__(self):
         """ Constructs the World """
-        self.setupBullet()
+        self.sky = SkyDome( "Resources/Models/skydome.egg" )
+        self.sky.sky.setScale( Vec3( 10,10,10))
+        self.setupPhysX()
         self.addGround()
+        self.setupLight()
+        self.car = Car( self.physxScene )
+        self.car.setSteer( -1.0 )
+        taskMgr.add(self.simulate, 'PhysX Simulation')
         
-    def setupBullet(self):
-        self.bulletWorld = BulletWorld()
-        self.bulletWorld.setGravity( Vec3( 0, 0, -9.81 ) )
+        self.debugNP = render.attachNewNode(self.physxScene.getDebugGeomNode())
+        self.debugNP.node().on()
+        self.debugNP.node().visualizeWorldAxes(True)
+        
+        self.keyControl = KeyControl()
+        #render.setShaderAuto() 
+        
+    def setupPhysX(self):
+        self.physx = PhysxManager.getGlobalPtr()
+        sceneDesc = PhysxSceneDesc()
+        sceneDesc.setGravity(Vec3(0, 0, -9.81))
+        self.physxScene = self.physx.createScene(sceneDesc)
+        m0 = self.physxScene.getMaterial(0)
+        m0.setRestitution(0.1)
+        m0.setStaticFriction(0.3)
+        m0.setDynamicFriction(0.3)
     
-    def addGround(self):        
-        shape = BulletPlaneShape(Vec3(0, 0, 1), 1)
-        node = BulletRigidBodyNode( 'Ground' )
-        node.addShape(shape)
-        np = render.attachNewNode(node)
-        np.setPos(0, 0, -2)
-        self.bulletWorld.attachRigidBody(node)    
+    def addGround(self):
+        groundShape = PhysxPlaneShapeDesc()
+        groundShape.setPlane( Vec3( 0, 0, 1 ), 0 );
+        groundActor = PhysxActorDesc();
+        groundActor.setName( 'ground' )
+        groundActor.addShape( groundShape )
+        self.ground = self.physxScene.createActor( groundActor )
+    
+    def setupLight(self):
+        self.light = render.attachNewNode(DirectionalLight("Spot")) 
+        self.light.node().setScene(render) 
+        self.light.node().setShadowCaster(True) 
+        render.setLight(self.light) 
+        
+    def simulate(self, task):
+        dt = globalClock.getDt()
+        self.physxScene.simulate(dt)
+        self.physxScene.fetchResults()
+        self.car.simulate()
+        self.keyControl.controlCar( self.car )
+        base.cam.lookAt( self.car.chassis.getGlobalPos() )
+        #print "num actors=" + str( self.physxScene.getNumActors() )
+        return task.cont
+    
 
-base.disableMouse()
-base.cam.setPos(0, -15, 0)
+#base.disableMouse()
+base.cam.setPos(10, -25, 5)
 base.cam.lookAt(0, 0, 0)
 
 world = World()
-car = Car( world.bulletWorld )
-
-def update( task ):
-    dt = globalClock.getDt()
-    world.bulletWorld.doPhysics( dt, 10, 1.0/180.0 )
-    return task.cont
-
-taskMgr.add( update, 'update' )
 
 run()
